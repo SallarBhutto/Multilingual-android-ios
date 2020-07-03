b6c1e71fe9671014291e6511fd9522299683da40
Sallar Bhutto <sallar@aciano.net>
2020-07-03 18:22:13 +0500

    package.json format

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-import React, { Component } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
+import React from 'react';
 import 'react-native-gesture-handler';
-import Navigation from './src/services/navigation'
+import Navigation from './src/services/navigation';
 
 function App() {
-  return (
-    <Navigation>
-    </Navigation>
-  );
+  return <Navigation />;
 }
 
 export default App;
